bb12cc6760d77465ad730afb90dd084427227dfe
Daniel <danielseniv@outlook.com>
2023-03-21 22:56:16 -0400

    Add button component, use classnames module to dynamically style all buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,17 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from "classnames"
+
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
+
+   return <>
+   <button disabled={props.disabled} onClick={props.onClick} className = {buttonClass}>
+      {props.children}
+   </button>
+   </>;
 }
