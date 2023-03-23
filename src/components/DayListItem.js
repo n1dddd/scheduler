0117ce8c726ee0512522ad5c858f980739fd1a5c
Daniel <danielseniv@outlook.com>
2023-03-23 17:51:39 -0400

    Add classNames, and use library to conditionally style component based on state (props passed down from test)

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import "components/DayListItem.scss";
+import classNames from "classnames";
 export default function DayListItem(props) {
+    const dayClass = classNames("dayListItem", {
+        "day-list__item": true,
+        "day-list__item--selected" : props.selected,
+        "day-list__item--full": props.spots === 0
+    })
+
     return(
-        <li onClick={() => props.setDay(props.name)}>
+        <li onClick={() => props.setDay(props.name)} className={dayClass}>
             <h2 className="text--regular">{props.Name}</h2>
             <h3 className="text-light">{props.spots} spots remaining</h3>
         </li>
