5cef5dcc306a0f4006945083626dc13431b7be8a
Daniel <danielseniv@outlook.com>
2023-03-26 16:52:26 -0400

    Add a selected identifier to the <li> element in component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -22,7 +22,7 @@ export default function DayListItem(props) {
     }
 
     return(
-        <li onClick={() => props.setDay(props.name)} className={dayClass}>
+        <li onClick={() => props.setDay(props.name)} className={dayClass} selected={props.selected}>
             <h2 className="text--regular">{props.Name}</h2>
             <h3 className="text-light">{formatSpots(props.spots)}</h3>
         </li>
