42b6041c933f4d7842bb2d67e149176c46128881
Daniel <danielseniv@outlook.com>
2023-04-01 21:31:26 -0400

    Add useEffect state handling, as well as modulizing state into object

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -63,7 +45,21 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday")
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+  
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}))
+
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then(response => {
+      setDays([...response.data])
+    })
+  },[])
 
   const appointment = Object.values(appointments).map((appointment) => {
     return (
@@ -83,9 +79,9 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList 
-          days={days}
-          value={day}
+          <DayList
+          days={state.days}
+          value={state.day}
           onChange={setDay}
           />
         </nav>
