effd96e58560b30e35c3d2a0d75b20c5bcd919d2
Daniel <danielseniv@outlook.com>
2023-04-03 02:51:34 -0400

    Add axios get requests to API server, change components to pulled api data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 // const appointments = {
 //   "1": {
@@ -49,7 +49,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
 
   let dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -61,16 +62,20 @@ export default function Application(props) {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
       axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
     ]).then((all) => {
-      setState(prev => ({...prev, days:all[0].data, appointments: all[1].data}))
+      setState(prev => ({...prev, days:all[0].data, appointments: all[1].data, interviewers:all[2].data}))
     })
   },[])
 
-  const appointment = dailyAppointments.map((appointment) => {
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
         {...appointment}
+        interview={interview}
       />
     )
   })
@@ -97,7 +102,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointment}
+        {schedule}
       </section>
     </main>
   );
