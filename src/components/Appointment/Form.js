6215495686cde7618803db281ae6abf5af566f66
Daniel <danielseniv@outlook.com>
2023-04-07 22:14:38 -0400

    Pass student and interview to onSave function, as result save state of new interview

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -14,7 +14,7 @@ export default function Form(props) {
     }
     const cancel = () => {
         reset();
-        return props.onCancel;
+        props.onCancel();
     }
 
     return(
@@ -23,7 +23,7 @@ export default function Form(props) {
                 <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
                     <input
                         className="appointment__create-input text--semi-bold"
-                        name="name"
+                        name={props.name}
                         type="text"
                         placeholder="Enter Student Name"
                         value={student}
@@ -32,14 +32,14 @@ export default function Form(props) {
                 </form>
                 <InterviewerList
                     interviewers={props.interviewers}
-                    onChange={setInterviewer}
                     value={interviewer}
+                    onChange={(e) => setInterviewer(e)}
                 />
             </section>
             <section className="appointment__card-right">
                 <section className="appointment__actions">
-                    <Button danger onClick={props.onCancel}>Cancel</Button>
-                    <Button confirm onClick={props.onSave}>Save</Button>
+                    <Button danger onClick={cancel}>Cancel</Button>
+                    <Button confirm onClick={(e) => props.onSave(student, interviewer)}>Save</Button>
                 </section>
             </section>
         </main>
