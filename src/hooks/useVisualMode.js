e2a2c34def91abd81ac32350c98a8c515b75c871
Daniel <danielseniv@outlook.com>
2023-04-07 22:21:10 -0400

    Refactor useVisualMode to not mutate state (use (prev) =>)

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,19 +1,20 @@
 import { useState} from "react";
 
 export default function useVisualMode(initial) {
-    const [mode, setMode] = useState(initial)
+    const [mode, setMode] = useState(initial);
     const [history, setHistory] = useState([initial]);
     //there was redundant state
     //make sure history is up to date, since mode relies on it now
 
     function transition(newMode, replace = false) {
-        if (replace === true) {
-            setMode((prev) => newMode);
+        if (replace) {
+            setMode((prev) => newMode)
             let replaceHistory = [...history]
+            replaceHistory[replaceHistory.length - 1] = mode;
             setHistory((prev) => replaceHistory)
         }
         else {
-            setMode((prev) => newMode);
+            setMode((prev) => newMode)
             let newHistory = [...history];
             newHistory.push(newMode);
             setHistory((prev) => newHistory);
@@ -21,15 +22,11 @@ export default function useVisualMode(initial) {
         //use prev to clone state, and then add mode with prev spread
     }
     function back() {
-        const backArray = [...history]
-        if (backArray.length === 0) {
-            setHistory(backArray);
-            return setMode(backArray[0])
-        }
-        else if (backArray.length > 1){
-        backArray.pop();
-        setMode(backArray[backArray.length - 1]);
-        setHistory(backArray);
+        let backArray = [...history]
+        backArray.pop(mode);
+        setHistory((prev) => backArray)
+        if (history.length > 1){
+        setMode((prev) => backArray[(backArray.length - 1)])
         }
     }
     return { mode, transition, back } 
