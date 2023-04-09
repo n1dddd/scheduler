6748ede9c36caca342c6c80e59bfa014b461f2e0
Daniel <danielseniv@outlook.com>
2023-04-08 22:31:17 -0400

    Change value from which state is set with

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,8 +5,8 @@ import Button from "components/Button";
 
 export default function Form(props) {
     
-    const [student, setStudent] = useState(props.student || "");
-    const [interviewer, setInterviewer] = useState(props.interviewer || null);
+    const [student, setStudent] = useState(props.name || "");
+    const [interviewer, setInterviewer] = useState(props.value || null);
 
     const reset = () => {
         setStudent("");
