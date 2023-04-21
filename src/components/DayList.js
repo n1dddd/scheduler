c54d83791f5774655a964aef5d02e58b7b86ccb6
Daniel <danielseniv@outlook.com>
2023-04-21 19:44:47 -0400

    Change syntax to spread day object as prop to DayListItem

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -7,10 +7,9 @@ export default function DayList(props) {
         return (
             <DayListItem
                 key={day.id}
-                name={day.name}
-                spots={day.spots}
                 selected={day.name === props.value}
                 setDay={props.onChange}
+                {...day}
             />
         )
     })
