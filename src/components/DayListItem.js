9d6ae434af01d0d7259339015b36814d6cab0fcf
Daniel <danielseniv@outlook.com>
2023-04-21 19:45:35 -0400

    Change logic for availability message i.e tracking number of spots in day

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,29 +2,29 @@ import React from 'react';
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
+const formatSpots = (spots) => {
+    if (!spots) {
+        return `no spots remaining`
+    } 
+    if (spots === 1){
+        return `1 spot remaining`
+    }
+    return `${spots} spots remaining`
+}
+
+
 export default function DayListItem(props) {
+
+    const availability = formatSpots(props.spots);
     const dayClass = classNames("dayListItem", {
         "day-list__item": true,
         "day-list__item--selected" : props.selected,
         "day-list__item--full": props.spots === 0
     })
-    const formatSpots = (spots) => {
-        if (spots === 0) {
-            const noSpots = "no spots remaining";
-            return noSpots;
-        } else if (spots === 1){
-            const oneSpot = "1 spot remaining"
-            return oneSpot;
-        } else {
-            const moreThanOneSpot = `${spots} spots remaining`;
-            return moreThanOneSpot;
-        }
-    }
-
     return(
-        <li onClick={() => props.setDay(props.name)} className={dayClass} selected={props.selected}>
+        <li onClick={() => props.setDay(props.name)} className={dayClass} data-testid="day">
             <h2 className="text--regular">{props.name}</h2>
-            <h3 className="text-light">{formatSpots(props.spots)}</h3>
+            <h3 className="text-light">{availability}</h3>
         </li>
     );
 }
